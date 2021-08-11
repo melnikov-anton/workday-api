19c57ff364e4a397fd1e87a02de3c00208c7ddda
Anton Melnikov <antokhamelnikov@yandex.ru>
2021-08-11 21:21:31 +0300

    Changed handler for home

--- internal/handlers/web.go
+++ internal/handlers/web.go
@@ -1,10 +1,17 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func HomePage(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, "Welcome to the home page")
+	if r.RequestURI != "/" {
+		rw.Header().Set("Content-Type", "text/html")
+		rw.WriteHeader(http.StatusNotFound)
+		rw.Write([]byte("<h4>Page not found</h4>"))
+		return
+	}
+	rw.Header().Set("Content-Type", "text/html")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("<h2>Welcome to the home page</h2>"))
 }
